d04df94344c84cdd6154718ff49363b4b0bdfb5e
Jay Wengrow <jaywngrw@gmail.com>
2015-06-07 16:01:26 -0500

    discounted items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,43 +1,53 @@
 class ProductsController < ApplicationController
 
-def index
-  @tacos = Product.all
-  if params[:sort] && params[:sort_order]
-    # Product.order(price: :desc)
-    # Product.order(:price => :desc)
-    @tacos = Product.order(params[:sort] => params[:sort_order])
+  def index
+    @tacos = Product.all
+    if params[:sort] && params[:sort_order]
+      # Product.order(price: :desc)
+      # Product.order(:price => :desc)
+      @tacos = @tacos.order(params[:sort] => params[:sort_order])
+    end
+    if params[:discount]
+      @tacos = @tacos.where("price < ?", 2)
+    end
   end
-end
 
-def show
-  @taco = Product.find_by(id: params[:id])
-end
+  def show
+    if params[:id] == "random"
+      tacos = Product.all
+      @taco = tacos.sample
+    else
+      @taco = Product.find_by(id: params[:id])
+    end
+  end
 
-def new
-end
-def create
-  @taco = Product.create(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
-  flash[:success] = "Taco made!"
-  redirect_to "/products/#{@taco.id}"
-end
+  def new
+  end
+  def create
+    @taco = Product.create(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
+    flash[:success] = "Taco made!"
+    redirect_to "/products/#{@taco.id}"
+  end
 
-def edit
-  @taco = Product.find_by(id: params[:id])
-end
+  def edit
+    @taco = Product.find_by(id: params[:id])
+  end
 
-def update
-  @taco = Product.find_by(id: params[:id])
-  @taco.update(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
-  flash[:success] = "This taco has been updated!"
-  redirect_to "/products/#{@taco.id}"
-end
+  def update
+    @taco = Product.find_by(id: params[:id])
+    @taco.update(id: params[:id], name: params[:name], price: params[:price], image: params[:image], description: params[:description], rating: params[:rating])
+    flash[:success] = "This taco has been updated!"
+    redirect_to "/products/#{@taco.id}"
+  end
 
-def destroy
-  @taco = Product.find_by(id: params[:id])
-  @taco.destroy
-  flash[:warning] = "Taco destroyed!"
-  redirect_to "/"
-end
+  def destroy
+    @taco = Product.find_by(id: params[:id])
+    @taco.destroy
+    flash[:warning] = "Taco destroyed!"
+    redirect_to "/"
+  end
+
+
 
 
 end
