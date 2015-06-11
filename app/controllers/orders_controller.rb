718e02275a8494af3b52715b6929d3863ece01ea
Jay Wengrow <jaywngrw@gmail.com>
2015-06-11 18:47:31 -0500

    saved subtotal, tax, and total in database

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,12 @@ class OrdersController < ApplicationController
   end
 
   def create
-    order = Order.create(user_id: current_user.id, quantity: params[:quantity])
+    quantity = params[:quantity].to_i
+    price = Product.find_by(id: params[:product_id]).price
+    subtotal = quantity * price
+    tax = subtotal * 0.09
+    total = subtotal + tax
+    order = Order.create(user_id: current_user.id, quantity: params[:quantity], product_id: params[:product_id], subtotal: subtotal, tax: tax, total: total)
     redirect_to "/orders/#{order.id}"
   end
 end
