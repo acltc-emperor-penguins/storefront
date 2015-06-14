b680fd4d75f0a39d6db68c711ba03b9c46e170ac
Jay Wengrow <jaywngrw@gmail.com>
2015-06-14 14:33:37 -0500

    added ability to filter by pork category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :orders
+  has_many :categorized_products
+  has_many :categories, through: :categorized_products
   belongs_to :supplier
 
   SALES_TAX = 0.09
