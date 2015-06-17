2588384ac7e1e1162171195566c2fa2b7c6456a5
Jay Wengrow <jaywngrw@gmail.com>
2015-06-17 18:27:17 -0500

    allow user to add product to shopping cart (database)

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,6 @@
 class CartedProductsController < ApplicationController
+  def create
+    CartedProduct.create(user_id: current_user.id, product_id: params[:product_id], quantity: params[:quantity], status: "carted")
+    redirect_to "/carted_products"
+  end
 end
