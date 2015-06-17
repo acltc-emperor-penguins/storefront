01879f434d698ea9dd554f116d6f1131dfc84ae2
Jay Wengrow <jaywngrw@gmail.com>
2015-06-17 18:44:43 -0500

    added buy now button to checkout page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,7 @@ class OrdersController < ApplicationController
   end
 
   def create
-    quantity = params[:quantity].to_i
-    price = Product.find_by(id: params[:product_id]).price
-
-    order = Order.new(user_id: current_user.id, quantity: params[:quantity], product_id: params[:product_id])
-    order.subtotal = order.calculate_subtotal(price)
-    order.tax = order.calculate_tax(price)
-    order.total = order.calculate_total(price)
-
-    order.save
+    
     redirect_to "/orders/#{order.id}"
   end
 end
