4c6484bba933852d7347bc9e76570edf79e40ff5
Jay Wengrow <jaywngrw@gmail.com>
2015-06-17 18:41:23 -0500

    created checkout page

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,4 +1,8 @@
 class CartedProductsController < ApplicationController
+  def index
+    @carted_products = current_user.carted_products.where(status: "carted")
+  end
+
   def create
     CartedProduct.create(user_id: current_user.id, product_id: params[:product_id], quantity: params[:quantity], status: "carted")
     redirect_to "/carted_products"
